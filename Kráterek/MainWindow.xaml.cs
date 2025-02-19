28e9817665d926f0be6301f0d799efe16edbb146
autowolf <autowolf@LAPTOP-LHVDE1AJ>
2025-02-19 10:42:16 +0100

    Add project files.

---
+++ Kráterek/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kráterek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Krater> lista = new List<Krater>();
+        public MainWindow()
+        {
+            beolvas();
+        }
+
+        private void beolvas()
+        {
+            using (var sr = new StreamReader("felszin.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var sor = sr.ReadLine().Split('\t');
+                    var krater = new Krater(double.Parse(sor[0]), double.Parse(sor[1]), double.Parse(sor[2]), sor[3]);
+                    lista.Add(krater);
+                }
+            }
+        }
+    }
+}
